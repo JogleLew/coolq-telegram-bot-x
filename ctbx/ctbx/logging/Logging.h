e5a56f462da388193ef4e9630fd36fd3f542d890
Mio <a771644612@live.com>
2019-02-16 01:43:39 +0800

    Fix a critical bug in logging
    Always use global variables with caution.
    
    在我这短暂的一生中
    
    我明白了全局变量是很容易踩坑的
    
    你究竟想说什么？
    
    下次要写单例模式啊JOJO!

--- ctbx/ctbx/logging/Logging.h
+++ ctbx/ctbx/logging/Logging.h
@@ -3,31 +3,24 @@
 #include "ctbx/stdafx.h"
 #include "Locale.h"
 
+#define LOGGER_NAME "ctbx_logger"
+
 namespace ctbx::logging {
-	static const std::string logger_name = "logger";
-	static const std::map<std::string, spdlog::level::level_enum> levels = {
-		{"debug", spdlog::level::level_enum::debug}, 
-		{"info", spdlog::level::level_enum::info},
-		{"warning",  spdlog::level::level_enum::warn}, 
-		{"error",  spdlog::level::level_enum::err },
-		{"critical", spdlog::level::level_enum::critical},
-		{"off", spdlog::level::level_enum::off},
-		{"trace", spdlog::level::level_enum::trace}
-	};
-	static std::shared_ptr<spdlog::logger> logger(static_cast<spdlog::logger*>(0));
 	inline void logger_initialize(const std::string& app_dir, int hour = 0, int minute = 0) {
-		logger = spdlog::daily_logger_mt(logger_name, app_dir, hour, minute);
-		logger->set_pattern("[%Y-%m-%d %H:%M:%S] [%l] %v");
-		logger->flush_on(spdlog::level::level_enum::trace);
-		logger->set_level(spdlog::level::level_enum::debug);
+		auto logger = spdlog::daily_logger_mt(LOGGER_NAME, app_dir, hour, minute);
+		spdlog::set_default_logger(logger);
+		spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [%l] %v");
+		spdlog::flush_on(spdlog::level::level_enum::trace);
+		spdlog::set_level(spdlog::level::level_enum::debug);
 	}
-	inline void log(const spdlog::level::level_enum level, const std::string& tag, const std::string& msg) { logger->log(level, "[{}] [{}]", tag, msg); }
+	
+	inline void log(const spdlog::level::level_enum level, const std::string& tag, const std::string& msg) { spdlog::log(level, "[{}] [{}]", tag, msg); }
 	inline void debug(const std::string& tag, const std::string& msg) { log(spdlog::level::debug, tag, msg); }
 	inline void info(const std::string& tag, const std::string& msg) { log(spdlog::level::info, tag, msg); }
 	inline void error(const std::string& tag, const std::string& msg) { log(spdlog::level::err, tag, msg); }
 	inline void warning(const std::string& tag, const std::string& msg) { log(spdlog::level::warn, tag, msg); }
 	inline void fatal(const std::string& tag, const std::string& msg) { log(spdlog::level::critical, tag, msg); }
-	inline void logger_drop() { spdlog::drop(logger_name); }
+	inline void logger_drop() { spdlog::drop(LOGGER_NAME); }
 }
 
 namespace cq::message {
